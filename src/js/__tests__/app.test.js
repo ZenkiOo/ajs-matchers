601516c474a71ed80c19398d63d0121bab17a7e1
ZenkiOo <zenky@list.ru>
2021-01-18 23:36:13 +0300

    fix test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -10,7 +10,7 @@ test('should show sorted array', () => {
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
       { name: 'мечник', health: 10 },
-    ];
+    ],
   result = sortCharsForHealth(chars);
 
   expect(result).toEqual(expected);
