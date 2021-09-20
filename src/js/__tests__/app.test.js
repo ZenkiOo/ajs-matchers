575670187086761b3afc1f9bb78b5051897dc871
ZenkiOo <zenky@list.ru>
2021-09-20 20:45:07 +0300

    fix app tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,18 +1,34 @@
-import sortCharsForHealth from '../app';
+import sortCharsForHealth from "../app";
 
-test('should show sorted array', () => {
-  const chars = [
-      { name: 'мечник', health: 10 },
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-    ],
-    expected = [
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-      { name: 'мечник', health: 10 },
-    ],
-  result = sortCharsForHealth(chars);
+describe("sortCharsForHealth", () => {
+  test("should show sorted array", () => {
+    const chars = [
+        { name: "мечник", health: 10 },
+        { name: "маг", health: 100 },
+        { name: "лучник", health: 80 },
+      ],
+      expected = [
+        { name: "маг", health: 100 },
+        { name: "лучник", health: 80 },
+        { name: "мечник", health: 10 },
+      ],
+      result = sortCharsForHealth(chars);
 
-  expect(result).toEqual(expected);
-  expect(result).not.toBe(expected);
+    expect(result).toEqual(expected);
+  });
+  test("should show sorted array (notToBe)", () => {
+    const chars = [
+        { name: "мечник", health: 10 },
+        { name: "маг", health: 100 },
+        { name: "лучник", health: 80 },
+      ],
+      expected = [
+        { name: "маг", health: 100 },
+        { name: "лучник", health: 80 },
+        { name: "мечник", health: 10 },
+      ],
+      result = sortCharsForHealth(chars);
+
+    expect(result).not.toBe(expected);
+  });
 });
